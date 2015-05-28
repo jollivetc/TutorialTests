67642b322547a172098583e6d05670e1bb8f1545
Christophe Jollivet <christophe.jollivet@gmail.com>
2015-05-28 06:53:48 +0200

    initial commit

---
+++ src/main/java/fr/jollivetc/tuto/tests/core/App.java
@@ -0,0 +1,13 @@
+package fr.jollivetc.tuto.tests.core;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
